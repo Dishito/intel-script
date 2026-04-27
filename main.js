b71b74070ad023a325647282f9fb65dde5fb0ec0
Dishito <diego.96.pereyra@gmail.com>
2026-04-26 22:57:14 -0300

    Se agrega lógica para extraer información del reporte que se está viendo.

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
+function validReport() {
+    if (!document.querySelector('.report_ReportAttack')) {
+        UI.ErrorMessage("Please, open a valid battle report.", 5000);
+        return false;
+    }
+    return true;
+}
+
+function extractDataFromReport() {
+    const report = {};
+    
+    const attacker = document.getElementById('attack_info_att');
+    const defender = document.getElementById('attack_info_def');
+    
+    // Atacante
+    const attLinks = attacker.querySelectorAll('a');
+    report.attacker = attLinks[0].textContent;
+    report.attackerVillage = attLinks[1].textContent.match(/(\d+)\|(\d+)/)[0];
+    
+    // Defensor
+    const defLinks = defender.querySelectorAll('a');
+    report.defender = defLinks[0].textContent;
+    report.defenderVillage = defLinks[1].textContent.match(/(\d+)\|(\d+)/)[0];
+    
+    console.log(report);
+}
+
 function main () {
     const params = new URLSearchParams(window.location.search);
     const screen = params.get('screen');
 
     switch (screen) {
         case 'report':
-            let check = params.has('view');
-            !check ? alert('Incorrect screen.') : alert('Ok')
-            UI.ErrorMessage(_t("verifyReportPage"), 5000);
+            validReport() ? extractDataFromReport() : null;
             break;
         default:
-            alert('Incorrect screen.');
+            UI.ErrorMessage("Incorrect screen.", 5000);
             break;
     }
 }
