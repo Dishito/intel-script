298e10969ce5f5b8d9b516d41d302d54086ccf82
Dishito <diego.96.pereyra@gmail.com>
2026-05-03 23:56:50 -0300

    New branch for extractData logic

--- main.js
+++ main.js
@@ -12,17 +12,16 @@ function extractDataFromReport() {
     //Report
     report.reportID = location.search.match(/view=(\d+)/)[1]; 
     report.time = document.querySelector('table.vis'); 
-    
-    const attacker = document.getElementById('attack_info_att');
-    const defender = document.getElementById('attack_info_def');
 
-    // Atacante
+    // Attacker
+    const attacker = document.getElementById('attack_info_att');
     const attLinks = attacker.querySelectorAll('a');
     report.attackerName = attLinks[0].textContent;
     report.attackerID = attLinks[0].href.match(/id=(\d+)/)[1];
     report.attackerVillage = attLinks[1].textContent.match(/(\d+)\|(\d+)/g)[0];
     
-    // Defensor
+    // Defender
+    const defender = document.getElementById('attack_info_def');
     const defLinks = defender.querySelectorAll('a');
     report.defenderName = defLinks[0].textContent;
     report.defenderID = defLinks[0].href.match(/id=(\d+)/)[1];
